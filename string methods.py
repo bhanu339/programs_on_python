07c3b2196681aeba073038f71a52176fb19dc951
bhanu Mannepalli <bhanumannepalli7@gmail.com>
2026-03-02 15:55:45 +0530

    Add files via upload

---
+++ string methods.py
@@ -0,0 +1,13 @@
+a='hai'
+b='hello'
+c='cse3'
+d='i like java'
+print(a.lower())
+print(b.isupper())
+print(a.isalpha())
+print(b.isdigit())
+print(a.upper())
+print(b.islower())
+print(c.isalnum())
+print(d.replace('python','java'))
+print(d.find('like'))
